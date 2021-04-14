b27743ca262cfdfee2cff56d1dc29c5cf4e02bc8
Makisuo <minetoaster12@gmail.com>
2021-04-14 23:57:39 +0200

    Remove old export

--- components/index.tsx
+++ components/index.tsx
@@ -1,4 +1,3 @@
-export { default as Chart } from './Chart/Chart'
 export { default as JsonInput } from './JSONInput'
 export { default as LoadingOverlay } from './LoadingOverlay'
 export { default as DragChart } from './DragChart'
