f5d218d2e3fbf802d3264f5d83aa162ae91b1594
Andreas-Zander <andreas.zander@transentis.com>
2021-05-31 15:31:08 +0200

    Logo changes. Fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,8 @@ import {
 	ButtonGroup,
 	AreaChart,
 	ResponsiveDragComponent as DragComponent,
-	StandardGridLayout,
 	Tabs,
+	StandardGridLayout,
 	DefaultGraphColors,
 	ResponsiveDoubleRangeSlider as Slider,
 	ThemeSwitcher,
@@ -79,17 +79,15 @@ const Home = (props: Props) => {
 	}
 
 	return (
-		<div
-			className='text-base-content'
-			style={{ width: '100vw', height: '100vh' }}
-		>
+		<div className='text-base-content min-h-screen'>
 			<Head>
 				<title>COVID-19 Simulation</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<div className='overflow-hidden h-full' style={{ width: '100%' }}>
+			<div className='overflow-hidden h-full w-full'>
 				<StandardGridLayout
 					dashboardTitle={'COVID-19 Simulation'}
+					logoDivCSS='logoDiv'
 					graphTabsComponent={
 						<ButtonGroup>
 							{graphs.map((mapEquatios, index) => (
