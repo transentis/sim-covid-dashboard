a254a715f6049e7575fa7b657d8140c923511a4b
Makisuo <minetoaster12@gmail.com>
2021-06-01 14:20:39 +0200

    Add new bptk-widget version

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
 						href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
 					/>
 				</Head>
-				<body>
+				<body className='bg-base-200'>
 					<Main />
 					<NextScript />
 				</body>
